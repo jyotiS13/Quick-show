91c620d309c37b39f433296411866ea028127841
Jyotrimayee Swain <jyotrimayeeswain017@gmail.com>
2025-08-30 12:46:54 +0530

    fix: Correct email field reference in user data creation for sync functions

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -14,7 +14,7 @@ const syncUserCreation = inngest.createFunction(
 
         const userData = {
             _id: id,
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             name: first_name + " " + last_name,
             image: image_url
         }
@@ -46,7 +46,7 @@ const syncUserUpdation = inngest.createFunction(
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             name: first_name + " " + last_name,
             image: image_url
         }
