11402bfad1a420e01fd1c95dd9f1050408ddcf26
Jyotrimayee Swain <jyotrimayeeswain017@gmail.com>
2025-08-30 12:06:24 +0530

    feat: Import email sending configuration for booking confirmation functionality

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,6 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js"
 import Booking from "../models/Booking.js";
 import showModel from "../models/Show.js";
+import sendEmail from "../config/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
